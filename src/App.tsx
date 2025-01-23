0f40fc31816c5f0a3358a8b2e6bf5a3a946979e9
helenatorrinha <helena.torrinha2002@gmail.com>
2025-01-23 16:06:31 +0000

    Added the Home component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import './styles/global.scss';
 import Navbar from './components/Navbar/Navbar';
 import AboutMe from './components/AboutMe/AboutMe';
 import WhatIsUGC from './components/WhatIsUGC/WhatIsUGC';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
-      <div className="center">
-        <h1>Portfolio</h1>
-      </div>
       <Navbar />
+      <Home />
       <AboutMe />
       <WhatIsUGC />
     </div>
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import styles from './Home.module.scss';
+
+const Home = () => {
+  return (
+    <section id="home" className={styles.section}>
+      <h1>Home</h1>
+      <p>
+        Welcome to the my website.
+      </p>
+    </section>
+  );
+};
+
+export default Home;
