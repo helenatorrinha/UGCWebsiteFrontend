c92ab41c997fe73f49941611ef4d1787a6adc3d8
helenatorrinha <helena.torrinha2002@gmail.com>
2025-01-23 15:58:29 +0000

    Updated font and navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,47 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
 import styles from './Navbar.module.scss';
+import iconImage from '../../assets/hlenamakeup.png';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // Detect scrolling to adjust the icon size
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li><Link to="about" smooth duration={100}>About Me</Link></li>
-        <li><Link to="what-is-ugc" smooth duration={100}>What is UGC</Link></li>
-      </ul>
-    </nav>
+    <div className={`${styles.container} ${isScrolled ? styles.smallNavBar : ''}`}>
+      <img
+        src={iconImage}
+        alt="Icon"
+        className={styles.icon}
+      />
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link to="about" smooth duration={100}>
+              ABOUT ME
+            </Link>
+          </li>
+          <li>
+            <Link to="what-is-ugc" smooth duration={100}>
+              WHAT IS UGC
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
