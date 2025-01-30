967ad9c1740623c942cc5d3f3307b0bdf0518309
helenatorrinha <helena.torrinha2002@gmail.com>
2025-01-30 17:10:27 +0000

    Added photo to the about  me page

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,4 +1,5 @@
 import styles from './AboutMe.module.scss';
+import aboutmeimage from '../../assets/aboutme.jpg';
 
 const AboutMe = () => {
   return (
@@ -8,6 +9,9 @@ const AboutMe = () => {
         Welcome to the About Me section. Here, you'll learn more about the inspiration
         behind this project and the passion that drives it.
       </p>
+      <div className={styles["photo-container"]}>
+            <img src={aboutmeimage} alt="About Me Imagine" />
+      </div>
     </section>
   );
 };
